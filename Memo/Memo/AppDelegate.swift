b275dc8fc67388ba3ebf926e807a218c7d4d3181
Tama <tamagawayuuma@icloud.com>
2020-11-22 17:22:43 +0900

    start

---
+++ Memo/Memo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Memo
+//
+//  Created by 玉川悠真 on 2020/11/22.
+//  Copyright © 2020年 玉川悠真. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Memo/Memo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Memo
+//
+//  Created by 玉川悠真 on 2020/11/22.
+//  Copyright © 2020年 玉川悠真. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
